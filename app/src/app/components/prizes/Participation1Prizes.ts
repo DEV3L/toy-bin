baad0ee5f709905effd128810fd81ab1362fa5f8
DEV3L <jus.beall@gmail.com>
2024-12-17 05:40:14 -0500

    Add new items

---
+++ app/src/app/components/prizes/Participation1Prizes.ts
@@ -0,0 +1,11 @@
+import { Prize } from "./prize";
+
+export const participation1Prizes: Prize[] = [
+  {
+    name: "I Ate My Grandma",
+    description: "When shit hits different",
+    image: "Season 3 - Demon/I Ate My Grandma.jpg",
+    rarity: "legendary",
+    available: true,
+  },
+];
